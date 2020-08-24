215c2760a2f13d7e29c379870401329d3502ab7a
Florian Moldenhauer <dracus@my.com>
2020-08-24 13:50:40 +0200

    arrays

---
+++ arrays/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+    for n in numbers.iter(){
+        println!{"{}",n};
+    }
+}
---
+++ arrays/target/rls/debug/deps/arrays-a001e485326ce898.d
@@ -0,0 +1,5 @@
+d:\Source\Rust Test\arrays\target\rls\debug\deps\arrays-a001e485326ce898.rmeta: src\main.rs
+
+d:\Source\Rust Test\arrays\target\rls\debug\deps\arrays-a001e485326ce898.d: src\main.rs
+
+src\main.rs:
