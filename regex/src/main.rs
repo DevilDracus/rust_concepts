f0e4c2a70d97f27904343847311c72f0b0e9b9e3
Florian Moldenhauer <dracus@my.com>
2020-08-24 14:36:37 +0200

    regex

---
+++ regex/src/main.rs
@@ -0,0 +1,16 @@
+extern crate regex;
+use regex::Regex;
+
+fn main() {
+    let re = Regex::new(r"(\w{5})").unwrap();
+
+    let text = "Hello";
+
+    //println!("Found match? {}",re.is_match(text));
+
+    match re.captures(text){
+        //Some(caps) => println!("Found match: {}", caps.get(0).unwrap().as_str()),
+        Some(caps) => println!("Found match: {}", &caps[0]),
+        None => println!("Couldn't find a match...")
+    }
+}
